938a417a2b41276b0ca30c4ce7f495ea203e9f1e
unknown <rootbrad86@gmail.com>
2018-03-21 06:40:17 -0400

    Initial commit

---
+++ pomodoro-api/src/main/java/com/pomodoro/api/PomodoroApiApplication.java
@@ -0,0 +1,20 @@
+package com.pomodoro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class PomodoroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PomodoroApiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello world!";
+    }
+}
