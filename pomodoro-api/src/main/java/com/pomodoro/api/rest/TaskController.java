59e8fcc49d54efd154e8277cc308985369829351
brad.root <brad.root@QSI1869.mshome.net>
2018-03-23 22:40:06 -0400

    Correcting app engine setup

--- pomodoro-api/src/main/java/com/pomodoro/api/rest/TaskController.java
+++ pomodoro-api/src/main/java/com/pomodoro/api/rest/TaskController.java
@@ -3,8 +3,10 @@ package com.pomodoro.api.rest;
 import com.pomodoro.api.model.Task;
 import java.util.Collection;
 import java.util.Collections;
+import javax.xml.ws.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,21 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
+ * Rest APi for dealing with the {@link Task} model.
  *
  * @author Brad Root
  */
 @RestController
-@RequestMapping("/{userId}/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     @GetMapping
-    public Collection<Task> getTask(@PathVariable(name = "userId") Integer userId) {
+    public Collection<Task> getTask() {
         return Collections.EMPTY_LIST;
     }
 
-    @PostMapping("/tasks")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Task> createTask(@PathVariable(name = "userId") Integer userId, @RequestBody Task task) {
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/1").buildAndExpand().toUri()).build();
     }
 
